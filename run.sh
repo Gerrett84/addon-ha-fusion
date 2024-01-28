12a1e86da127413f6a0fd1a66533ba996fbc484f
Gerrett84 <gerrett84@gmail.com>
2024-01-28 22:33:36 +0100

    readd hass_url

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
+HASS_URL=$(bashio::config 'hass_url') 
+export HASS_URL
 export HASS_PORT=$(bashio::core.port)
 export EXPOSED_PORT=$(bashio::addon.port "8099/tcp")
 
